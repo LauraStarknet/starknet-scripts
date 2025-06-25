5932653fb13754bf56a7a2ed57692586d6e12127
LauraStarknet <laura@starknet.org>
2025-06-25 16:24:40 +0200

    heading7

--- headings7.js
+++ headings7.js
@@ -1,6 +1,5 @@
 function createHeadingsAll() {
     const headings = document.querySelectorAll('[data-split="heading"]');
-    const headingsSmall = document.querySelectorAll('.animated-heading-scroll, .heading-small');
     headings.forEach(heading => {
         const customStart = heading.getAttribute('data-trigger-start');
         const scrollTriggerStart = customStart || "top 70%";
@@ -16,7 +15,7 @@ function createHeadingsAll() {
                         once: true,
                     }
                 }).from(split.lines, {
-                   opacity: 1,
+                    opacity: 1,
                     y: 0,
                     stagger: 0.05,
                     duration: 0.75,
@@ -26,6 +25,7 @@ function createHeadingsAll() {
             }
         });
     });
+    const headingsSmall = document.querySelectorAll('.animated-heading-scroll, .heading-small');
 
     headingsSmall.forEach(heading => {
         const customStart = heading.getAttribute('data-trigger-start');
@@ -35,19 +35,24 @@ function createHeadingsAll() {
             charsClass: 'letter',
             autoSplit: true,
             onSplit(split) {
+                // gsap.set(split.chars, {
+                //     opacity: 0,
+                //     y: 20,
+                // });
                 return gsap.timeline({
                     scrollTrigger: {
                         trigger: heading,
                         start: scrollTriggerStart,
                         once: true,
                     }
-                }).from(split.lines, {
-                    yPercent: 100,
-                    delay: 0.25,
+                }).from(split.chars, {
                     opacity: 0,
-                    duration: 1,
-                    ease: "expo.out",
-                    stagger: 0.1,
+                    y: 20,
+                    stagger: 0.04,
+                    duration: 0.75,
+                    delay: 0.5,
+                    ease: 'power2.out',
+                    filter: 'blur(10px)'
                 });
             }
         });
