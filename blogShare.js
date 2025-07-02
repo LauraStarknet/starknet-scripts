7a4bb026c6e7313b87d88c57db4b325aa4d2ba38
LauraStarknet <laura@starknet.org>
2025-07-02 14:05:03 +0200

    blogShare.js

---
+++ blogShare.js
@@ -0,0 +1,15 @@
+function addShareLinks2() {
+    if(document.querySelector('.social-link--x')) {
+        document.querySelector('.social-link--x').href = `https://twitter.com/intent/tweet?url=${window.location.href}`
+        document.querySelector('.social-link--discord').href = `https://twitter.com/intent/tweet?url=${window.location.href}`
+        document.querySelector('.social-link--facebook').href = `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`
+        document.querySelector('.social-link--linkedin').href = `http://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}`
+        document.querySelector('.social-link--copy').addEventListener('click', () => {
+            navigator.clipboard.writeText(window.location.href);
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    addShareLinks()
+})
