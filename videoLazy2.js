bbe184a79b8b91f9879aa06ef9674c003ffa0716
LauraStarknet <laura@starknet.org>
2025-07-04 10:32:05 +0200

    1280

--- videoLazy2.js
+++ videoLazy2.js
@@ -4,8 +4,8 @@ function handleVideoIntersection(entries) {
     entries.map((entry) => {
         if (entry.isIntersecting) {
             entry.target.parentElement.classList.add('currently-in-view');
-            if(window.innerWidth > 1440 && entry.target.className.indexOf('desktop') > -1) entry.target.play()
-            if(window.innerWidth < 1440 && entry.target.className.indexOf('tablet') > -1) entry.target.play()
+            if(window.innerWidth > 1280 && entry.target.className.indexOf('desktop') > -1) entry.target.play()
+            if(window.innerWidth < 1280 && entry.target.className.indexOf('tablet') > -1) entry.target.play()
         } else {
             entry.target.parentElement.classList.remove('currently-in-view');
             entry.target.pause()
