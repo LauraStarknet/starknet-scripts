dba91746b7bbfd7918e3b8c24e6f744830399e05
LauraStarknet <laura@starknet.org>
2025-07-09 10:49:16 +0200

    transition

--- transition.js
+++ transition.js
@@ -10,7 +10,7 @@ function hideGrid() {
 
 function loadTransition() {
     showGrid()
-    let timeOut = window.location.hash = '/' ? 2500:10;
+    let timeOut = window.location.hash = '/' ? 1500:10;
     setTimeout(() => hideGrid(), timeOut);
 
     // Pre-process all valid links
