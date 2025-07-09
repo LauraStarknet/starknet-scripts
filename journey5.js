259b173e9679ef6f017b47c8928f78332a70d170
LauraStarknet <laura@starknet.org>
2025-07-09 18:58:13 +0200

    journey5

--- journey5.js
+++ journey5.js
@@ -11,7 +11,7 @@ function calculateVideosJourney() {
             if(i === 3 && currentScroll > vh * 3) {
                 let percent = (currentScroll - vh * 3) / vh
                 allVideos[i].style.transform = `translateY(${currentScroll - (vh * i)}px) scale(${1+(touch ? 1:3.5)*percent})`;
-                allVideos[i].style.filter = `blur(${(touch ? 10:20)*percent}px)`;
+                if(!touch) allVideos[i].style.filter = `blur(${(touch ? 10:20)*percent}px)`;
                 allVideos[i].style.opacity = 1-(0.2*percent);
             }
         }
